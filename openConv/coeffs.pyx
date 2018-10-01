5e77f8203b17008498cf2dd0f889faac452b6785
Oliver Haas <oliver.sebastian.haas@googlemail.com>
2018-10-01 15:37:41 +0200

    Readme and doc rework.

--- openConv/coeffs.pyx
+++ openConv/coeffs.pyx
@@ -1,6 +1,7 @@
 
 import os
 import numpy as np
+import cython as cy
 
 
 dataDir = os.path.dirname(__file__) + 'coeffsData/'
@@ -8,13 +9,14 @@ coeffsAllDict = {}
 allFileNames = os.listdir(dataDir)
 allFileNames = sorted(allFileNames)
 
-while len(allFileNames) > 0:
-    currentCoeffsName = allFileNames[0][:-7]
-    currentCoeffsDict = {}
-    coeffsAllDict[currentCoeffsName] = currentCoeffsDict
-    while len(allFileNames) > 0 and currentCoeffsName == allFileNames[0][:-7]:
-        currentCoeffsDict[int(allFileNames[0][-6:-4])] = np.load(dataDir + allFileNames[0]).astype(np.double)
-        allFileNames.remove(allFileNames[0])
+with cy.wraparound(True):
+    while len(allFileNames) > 0:
+        currentCoeffsName = allFileNames[0][:-7]
+        currentCoeffsDict = {}
+        coeffsAllDict[currentCoeffsName] = currentCoeffsDict
+        while len(allFileNames) > 0 and currentCoeffsName == allFileNames[0][:-7]:
+            currentCoeffsDict[int(allFileNames[0][-6:-4])] = np.load(dataDir + allFileNames[0]).astype(np.double)
+            allFileNames.remove(allFileNames[0])
 
 
 # Actual function to get the coefficients
